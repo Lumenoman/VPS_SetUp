0f59bbde6309679f1c4378a8115a878f8df79694
Lumenoman <Lumenoman@protonmail.com>
2026-07-20 23:09:24 +0300

    Fix symbolic link and update installation messages
    Updated symbolic link and echo messages for VPS_SetUp.

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ curl -fsSL \
 https://raw.githubusercontent.com/Lumenoman/VPS_SetUp/main/VPS_SetUp.sh \
 -o /usr/local/bin/VPS_SetUp/VPS_SetUp.sh
 chmod +x /usr/local/bin/VPS_SetUp/VPS_SetUp.sh
-ln -s /usr/local/bin/VPS_SetUp/VPS_SetUp.sh /usr/local/bin/VPS_SetUp
-echo "✓ VPS_SetUp установлен"
+ln -s /usr/local/bin/VPS_SetUp/VPS_SetUp.sh /usr/local/bin/VPSSetUp
+echo "✓ VPS_SetUp.sh установлен"
 echo "Запуск:"
-echo "VPS_SetUp"
+echo "VPSSetUp"
 }
+install_script
