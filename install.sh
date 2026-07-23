db2dddf8ee83d4fe7685053419d8880757be547b
Lumenoman <Lumenoman@protonmail.com>
2026-07-24 00:38:38 +0300

    Update installation message for VPS_SetUp.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ https://raw.githubusercontent.com/Lumenoman/VPS_SetUp/main/VPS_SetUp.sh \
 -o /usr/local/bin/VPS_SetUp/VPS_SetUp.sh
 chmod +x /usr/local/bin/VPS_SetUp/VPS_SetUp.sh
 ln -sf /usr/local/bin/VPS_SetUp/VPS_SetUp.sh /usr/local/bin/VPSSetUp
-echo "✓ VPS_SetUp.sh установлен"
+echo "✓ VPS_SetUp.sh установлен/обновлен"
 echo "Запуск:"
 echo "VPSSetUp"
 }
